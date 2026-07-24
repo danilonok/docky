eb2b40ae689b039af3223ead1dba7a4c2eeb2096
danilonok <danilonok@DANILONOKGM>
2026-07-24 21:40:42 +0200

    Double quotes inside f string

--- app/tasks/celery.py
+++ app/tasks/celery.py
@@ -2,7 +2,7 @@ from celery import Celery
 import os
 
 app = Celery('proj',
-             broker=f'amqp://{os.environ.get('RABBIT_MQ_HOST')}:5672/',
+             broker=f'amqp://{os.environ.get("RABBIT_MQ_HOST")}:5672/',
              backend="rpc://",
              include=['app.tasks.tasks'])
 
