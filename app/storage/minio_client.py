9ce7bf5844da15b3e36d2baa2876be2dacd785be
danilonok <danilonok@DANILONOKGM>
2026-07-24 02:43:44 +0200

    Deleted dot from the url

--- app/storage/minio_client.py
+++ app/storage/minio_client.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 s3 = boto3.client(
     "s3",
-    endpoint_url=f"http://{os.environ.get('AWS_HOST')}.:9000",
+    endpoint_url=f"http://{os.environ.get('AWS_HOST')}:9000",
     aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
     aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
     region_name="us-east-1"
