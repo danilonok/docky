bff843786b67542691891cd1f33cb7da77894ef0
danilonok <danilonok@DANILONOKGM>
2026-07-29 21:23:20 +0200

    Added CORS for local development

--- app/main.py
+++ app/main.py
@@ -13,15 +13,32 @@ from app.auth.helpers import ACCESS_TOKEN_EXPIRE_MINUTES, Token
 
 from app.dependencies.auth import authenticate_user, create_access_token
 
+from fastapi.middleware.cors import CORSMiddleware
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     yield
 
 
-
-
 app = FastAPI(lifespan=lifespan)
+
+origins = [
+    "http://localhost",
+    "http://localhost:8980",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+
 
 app.include_router(users.router)
 app.include_router(chats.router)
