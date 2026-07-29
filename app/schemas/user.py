73c37def39998d62bcd6e6392af003c2e31aa14b
danilonok <danilonok@DANILONOKGM>
2026-07-29 20:39:09 +0200

    add password checks

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,6 +1,8 @@
 # Pass-hash should be shown in read
 
-from pydantic import BaseModel, ConfigDict, Field
+from typing import Annotated
+
+from pydantic import AfterValidator, BaseModel, ConfigDict, Field, field_validator, validator
 
 from pydantic import EmailStr
 
@@ -10,8 +12,20 @@ class UserBase(BaseModel):
     disabled: bool = False
 
 
+SPECIAL_CHARACTERS = "!@#$%^&*()-_=+[]{};:,.<>?/\\|`~'\""
+
+
+def check_password(value: str) -> str:
+    if not any(c.isupper() for c in value):
+        raise ValueError("Password must contain uppercase letter")
+    if not any(c.isdigit() for c in value):
+        raise ValueError("Password must contain digit")
+    if not any(c in SPECIAL_CHARACTERS for c in value):
+        raise ValueError("Password must contain special character")
+    return value
+
 class UserCreate(UserBase):
-    password: str
+    password: Annotated[str, Field(min_length=8), AfterValidator(check_password)]
     pass
 
 class UserUpdate(UserBase):
